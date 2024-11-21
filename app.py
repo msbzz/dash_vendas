6703845e3144c1f6e2622c997c20e649d2ec8f65
Marco <mbarozzi@gmail.com>
2024-11-21 09:53:50 -0300

    preenchedo o primeiro grafico

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ file_path = os.path.join(os.path.dirname(__file__), 'supermarket_sales.csv')
 df_data = pd.read_csv(file_path)
 df_data['Date']=pd.to_datetime(df_data['Date'])
 
-df_data['City'].value_counts().index 
-
 #============ Layout ================#
 
 app.layout = html.Div(
@@ -26,15 +24,15 @@ app.layout = html.Div(
 
       html.H5('Cidades'),
       dcc.Checklist(df_data['City'].value_counts().index,
-                    df_data['City'].value_counts().index,
-                    id='check-city'),
+                    df_data['City'].value_counts().index,inline=True,
+                    id='check_city'),
 
       html.H5('Variável de análise'),
 
       dcc.RadioItems(
          options= ['gross income','Rating'],
          value= 'gross income',
-         id='main-variable',
+         id='main_variable',
          inline=True
           ),    
 
@@ -50,8 +48,29 @@ app.layout = html.Div(
 
 #============ CallBacks ================#
 
-
-
+@app.callback(
+    #[
+      Output(component_id='city-fig', component_property='figure'),
+      # Output(component_id='pay-fig', component_property='figure'),
+      # Output(component_id='income_per_product', component_property='figure')
+    #],
+    [
+      Input(component_id='check_city', component_property='value'),
+      Input(component_id='main_variable', component_property='value'),
+    ])
+def render_graphs(cities,main_variable):
+  
+ 
+  #aqui define se é um somatorio ou media
+  operation = np.sum if main_variable=='gross income' else np.mean
+  #filtra apenas a lista de cidades
+  df_filtered = df_data[df_data['City'].isin(cities)]
+  # agrupar por cidade
+  df_city = df_filtered.groupby('City')[main_variable].apply(operation).to_frame().reset_index()
+    
+  fig_city = px.bar(df_city,x='City',y=main_variable)
+  
+  return fig_city
 #============ Run server ================#
 
 if __name__ == "__main__":
